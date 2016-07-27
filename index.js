ea03737e82e2ab7b952911c8d0f0ad8088b7a7e7
Madeleine Sheehan <madeleine.f.sheehan@gmail.com>
2016-07-27 22:23:11 +0000

    edits to index.js read from file

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 var express = require('express')
 var app = express()
 var fs = require('fs')
-var filename = "index.html"
-
+var filename = "./index.html"
+var text = fs.readFileSync(filename, 'utf8')
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 
 app.get('/', function(request, resp) {
-    var text = fs.readFileSync(filename, "utf8", function(err, data)) 
-        resp.send(text);
+    resp.send(text);
     
 });
 
