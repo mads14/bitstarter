c42d4f328356ac9d8ea1127683120be32597dc70
Madeleine Sheehan <madeleine.f.sheehan@gmail.com>
2016-07-27 21:50:39 +0000

    added index.html

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 var express = require('express')
 var app = express()
+var fs = require('fs')
+var filename = "./index.html"
+
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World2!')
-})
+
+app.get('/', function(request, resp) {
+    resp.writeHead(200, {
+        "Content-Type": "text/html"
+    });
+    fs.readFileSync(filename, "utf8", function(err, data) {
+        if (err) throw err;
+        resp.write(data);
+        resp.end();
+    });
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
